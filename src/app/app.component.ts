763bba913802f752bea6490cf9f025c1ccaf308b
Surendiran <surendiran@leaguex.com>
2020-04-03 09:05:17 +0530

    ui updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, time: "2014-02-17, 19:33:51", type: 2456, source: "bnsf.bnsfi.9299.itc", destination: "bnsf.bnsfi.9299.itc", summary: "Head End Milepost: 10033706 | Head End Track Name: MT 1 | Rear End Milepost: 10033110 | Rear End" },
+  { position: 2, time: "2014-02-17, 19:33:51", type: 2456, source: "bnsf.bnsfi.9299.itc", destination: "bnsf.bnsfi.9299.itc", summary: "Head End Milepost: 10033706 | Head End Track Name: MT 1 | Rear End Milepost: 10033110 | Rear End" },
+  { position: 3, time: "2014-02-17, 19:33:51", type: 2456, source: "bnsf.bnsfi.9299.itc", destination: "bnsf.bnsfi.9299.itc", summary: "Head End Milepost: 10033706 | Head End Track Name: MT 1 | Rear End Milepost: 10033110 | Rear End" },
+  { position: 4, time: "2014-02-17, 19:33:51", type: 2456, source: "bnsf.bnsfi.9299.itc", destination: "bnsf.bnsfi.9299.itc", summary: "Head End Milepost: 10033706 | Head End Track Name: MT 1 | Rear End Milepost: 10033110 | Rear End" },
+  { position: 5, time: "2014-02-17, 19:33:51", type: 2456, source: "bnsf.bnsfi.9299.itc", destination: "bnsf.bnsfi.9299.itc", summary: "Head End Milepost: 10033706 | Head End Track Name: MT 1 | Rear End Milepost: 10033110 | Rear End" },
+  { position: 6, time: "2014-02-17, 19:33:51", type: 2456, source: "bnsf.bnsfi.9299.itc", destination: "bnsf.bnsfi.9299.itc", summary: "Head End Milepost: 10033706 | Head End Track Name: MT 1 | Rear End Milepost: 10033110 | Rear End" },
+  { position: 7, time: "2014-02-17, 19:33:51", type: 2456, source: "bnsf.bnsfi.9299.itc", destination: "bnsf.bnsfi.9299.itc", summary: "Head End Milepost: 10033706 | Head End Track Name: MT 1 | Rear End Milepost: 10033110 | Rear End" },
+  { position: 8, time: "2014-02-17, 19:33:51", type: 2456, source: "bnsf.bnsfi.9299.itc", destination: "bnsf.bnsfi.9299.itc", summary: "Head End Milepost: 10033706 | Head End Track Name: MT 1 | Rear End Milepost: 10033110 | Rear End" },
+  { position: 9, time: "2014-02-17, 19:33:51", type: 2456, source: "bnsf.bnsfi.9299.itc", destination: "bnsf.bnsfi.9299.itc", summary: "Head End Milepost: 10033706 | Head End Track Name: MT 1 | Rear End Milepost: 10033110 | Rear End" }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   title = 'my-app2';
+  displayedColumns: string[] = ["position", 'time', 'type', 'source', 'destination', 'summary'];
+  dataSource = ELEMENT_DATA;
 }
